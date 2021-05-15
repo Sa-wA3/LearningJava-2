e210f58cc5570819596ecfb61c5b67440d1979b2
Takayuki Hirasawa <atware_hirasawa@TakayukinoMacBook-Pro.local>
2021-05-16 00:04:52 +0900

    completed Chap14

---
+++ Chap14/player/DVDPlayerTester.java
@@ -0,0 +1,9 @@
+public class DVDPlayerTester {
+    
+    public static void main(String[] args) {
+        DVDPlayer a = new DVDPlayer();
+        a.play();
+        a.stop();
+        a.slow();
+    }
+}
